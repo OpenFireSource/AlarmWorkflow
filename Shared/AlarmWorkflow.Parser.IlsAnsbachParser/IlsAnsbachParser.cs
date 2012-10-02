f458aace6aa48befaef052eac998d74579427efd
chrishensel <futurefwarrior@googlemail.com>
2012-10-02 21:45:46 +0200

    Updated OCR- and parsing-logic (will split multipage-tiffs into distinct files and scan those then, Changed projects to x86 instead of Any CPU (doesn't matter at all and allows us to debug and Edit-and-continue!)

--- Shared/AlarmWorkflow.Parser.IlsAnsbachParser/IlsAnsbachParser.cs
+++ Shared/AlarmWorkflow.Parser.IlsAnsbachParser/IlsAnsbachParser.cs
@@ -1,5 +1,7 @@
 using AlarmWorkflow.Shared.Core;
 using AlarmWorkflow.Shared.Extensibility;
+using System;
+using System.Collections.Generic;
 
 namespace AlarmWorkflow.Parser.IlsAnsbachParser
 {
@@ -8,6 +10,16 @@ namespace AlarmWorkflow.Parser.IlsAnsbachParser
     /// </summary>
     public class IlsAnsbachParser : IParser
     {
+        #region Fields
+
+        /// <summary>
+        /// Dictionary that contains text parsers which differ in each section. This is due to the fact that the ILS-Ansbach-faxes are a little complex
+        /// and in order to maintain readability.
+        /// </summary>
+        private Dictionary<CurrentSection, Func<string, bool>> _sectionParsers;
+        
+        #endregion
+
         #region Constructors
 
         /// <summary>
@@ -17,19 +29,79 @@ namespace AlarmWorkflow.Parser.IlsAnsbachParser
         /// <param name="replaceList">The RreplaceList object.</param>
         public IlsAnsbachParser()
         {
+            // Set up section parsers
+            _sectionParsers = new Dictionary<CurrentSection, Func<string, bool>>();
+            _sectionParsers[CurrentSection.AHeader] = HeaderParser;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private bool HeaderParser(string line)
+        {
+            return true;
         }
 
         #endregion
 
         #region IParser Members
 
-        Operation IParser.Parse(string[] file)
+        Operation IParser.Parse(string[] lines)
         {
-            Operation einsatz = new Operation();
+            Operation operation = new Operation();
 
             // TODO
+            try
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
 
-            return einsatz;
+
+                }
+                CurrentSection section = CurrentSection.AHeader;
+                foreach (string line in lines)
+                {
+                    string msg;
+                    string prefix;
+                    int x = line.IndexOf(':');
+                    if (x == -1)
+                    {
+                        continue;
+                    }
+
+                    prefix = line.Substring(0, x);
+                    msg = line.Substring(x + 1).Trim();
+
+                    prefix = prefix.Trim().ToUpperInvariant();
+
+                }
+            }
+            catch (System.Exception ex)
+            {
+
+            }
+
+            return operation;
+        }
+
+        #endregion
+
+        #region Nested types
+
+        private enum CurrentSection
+        {
+            AHeader,
+            BMitteiler,
+            CEinsatzort,
+            DZielort,
+            EEinsatzgrund,
+            FEinsatzmittel,
+            GBemerkung,
+            /// <summary>
+            /// Footer text. Introduced by "ENDE FAX". Can be ignored completely.
+            /// </summary>
+            HFooter,
         }
 
         #endregion
