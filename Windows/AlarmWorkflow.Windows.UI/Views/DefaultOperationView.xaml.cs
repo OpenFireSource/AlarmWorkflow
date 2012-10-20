a65341632864ced0f763f97d6263fa1c91735901
chrishensel <futurefwarrior@googlemail.com>
2012-10-20 16:06:41 +0200

    DefaultOperationView now displays RoutePlanImage as intended

--- Windows/AlarmWorkflow.Windows.UI/Views/DefaultOperationView.xaml.cs
+++ Windows/AlarmWorkflow.Windows.UI/Views/DefaultOperationView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using AlarmWorkflow.Shared.Core;
 using AlarmWorkflow.Windows.UI.Extensibility;
 using AlarmWorkflow.Windows.UI.ViewModels;
@@ -70,11 +73,24 @@ namespace AlarmWorkflow.Windows.UI.Views
                 get { return _operation; }
                 set
                 {
+                    if (value == _operation)
+                    {
+                        return;
+                    }
+
                     _operation = value;
 
-                    // Set operation itself
                     OnPropertyChanged("Operation");
+                    OnPropertyChanged("RoutePlanImage");
                 }
+            }
+
+            /// <summary>
+            /// Gets the image of the route plan (if available).
+            /// </summary>
+            public ImageSource RoutePlanImage
+            {
+                get { return GetRoutePlanImage(); }
             }
 
             #endregion
@@ -87,6 +103,29 @@ namespace AlarmWorkflow.Windows.UI.Views
             public ViewModel()
             {
 
+            }
+
+            #endregion
+
+            #region Methods
+
+            private ImageSource GetRoutePlanImage()
+            {
+                if (_operation == null)
+                {
+                    return null;
+                }
+                if (_operation.RouteImage == null)
+                {
+                    // Return dummy image
+                    return Helper.GetNoRouteImage();
+                }
+
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.StreamSource = new MemoryStream(_operation.RouteImage);
+                image.EndInit();
+                return image;
             }
 
             #endregion
