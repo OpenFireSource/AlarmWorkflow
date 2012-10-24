d50d7089cd0f4cfd0c9d74ca6542a96824925fae
chrishensel <futurefwarrior@googlemail.com>
2012-10-24 21:06:29 +0200

    (Added missing project)

---
+++ Windows/AlarmWorkflow.Windows.UI.Contracts/Security/AuthorizationMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AlarmWorkflow.Windows.UI.Contracts.Security
+{
+    /// <summary>
+    /// Specifies the supported authorization modes.
+    /// </summary>
+    public enum AuthorizationMode
+    {
+        /// <summary>
+        /// Authorization requires the user to confirm the dialog.
+        /// </summary>
+        SimpleConfirmation,
+        /// <summary>
+        /// Authorization uses a password-system.
+        /// </summary>
+        Password,
+    }
+}
