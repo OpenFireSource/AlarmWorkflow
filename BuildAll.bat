287806c56a68082c02104c96aff4bcc331843262
chrishensel <futurefwarrior@googlemail.com>
2012-11-13 18:23:54 +0100

    Updated build script

--- BuildAll.bat
+++ BuildAll.bat
@@ -4,17 +4,20 @@ msbuild Shared\AlarmWorkflow.Shared.sln /p:Configuration=Debug /verbosity:minima
 msbuild Shared\AlarmWorkflow.Shared.Parser.sln /p:Configuration=Debug /verbosity:minimal
 msbuild Shared\AlarmWorkflow.Shared.Jobs.sln /p:Configuration=Debug /verbosity:minimal
 
-echo 
+echo -------------------------------------------------
+echo
 echo Build Windows-specific stuff...
 msbuild Windows\AlarmWorkflow.Windows.sln /p:Configuration=Debug /verbosity:minimal
 msbuild Windows\AlarmWorkflow.Windows.Configuration.sln /p:Configuration=Debug /verbosity:minimal
 
+echo -------------------------------------------------
 echo
 echo Build Website(s)...
 msbuild Website\AlarmWorkflow.Website.Asp\AlarmWorkflow.Website.Asp.sln /p:Configuration=Debug /verbosity:minimal
 
+echo -------------------------------------------------
 echo
 echo Copy Files for the Setup
-copy "Build" "Installer"
+robocopy "Build" "Installer\Build" /XC /s
 
 pause
