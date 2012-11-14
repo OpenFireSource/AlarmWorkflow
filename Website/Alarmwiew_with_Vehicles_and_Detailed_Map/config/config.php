e0bc8332c0eb034161c3b8b937d07934465b69c3
Christian Ha. <github@machnet.de>
2012-11-14 07:47:32 +0100

    Add the alarmview improved website
    there is an detailed google Map for the destination area and a field
    added at the bottom for the needed vehicles.

---
+++ Website/Alarmwiew_with_Vehicles_and_Detailed_Map/config/config.php
@@ -0,0 +1,11 @@
+<?php
+        /* Database Settings */
+        $db_server = "localhost";
+        $db_user = "root";
+        $db_pwd = "setyourpassword";
+        $db_database = "alarmworkflow";
+
+        /* Website Settings */
+        $ws_title = "Fahrzeughalle";
+        $ws_version = "0.1";
+?>
