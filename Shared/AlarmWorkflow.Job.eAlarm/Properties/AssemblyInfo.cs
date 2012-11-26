9f6d93ac59a74d96e312902337b65c6697d8b3bb
chrishensel <futurefwarrior@googlemail.com>
2012-11-26 21:29:06 +0100

    Updated version of assemblies
    0.6.1.0

--- Shared/AlarmWorkflow.Job.eAlarm/Properties/AssemblyInfo.cs
+++ Shared/AlarmWorkflow.Job.eAlarm/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyFileVersion("0.6.1.0")]
