9fb2db9e7f509b0a991c27e4f2a38afb93232c19
Florian Ritterhoff <florian.olching@gmail.com>
2012-11-27 14:50:32 +0100

    UAC + Service Install

--- Tools/AlarmWorkflow.AutoUpdater/Utilities.cs
+++ Tools/AlarmWorkflow.AutoUpdater/Utilities.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Principal;
 using System.Windows.Forms;
 
 namespace AlarmWorkflow.Tools.AutoUpdater
@@ -15,6 +16,16 @@ namespace AlarmWorkflow.Tools.AutoUpdater
         {
             DialogResult dr = MessageBox.Show(text, "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             return dr == DialogResult.Yes;
+        }
+        internal static bool IsElevated()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            if (id != null)
+            {
+                WindowsPrincipal p = new WindowsPrincipal(id);
+                return p.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            return false;
         }
     }
 }
