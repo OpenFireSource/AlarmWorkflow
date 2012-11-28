fe11fe85b337fab5327b425886f37405040ab4d9
chrishensel <futurefwarrior@googlemail.com>
2012-11-28 20:52:08 +0100

    Added commentaries

--- Shared/AlarmWorkflow.Job.MailingJob/MailingEntryObject.cs
+++ Shared/AlarmWorkflow.Job.MailingJob/MailingEntryObject.cs
@@ -4,11 +4,32 @@ using AlarmWorkflow.Shared.Diagnostics;
 
 namespace AlarmWorkflow.Job.MailingJob
 {
-    public class MailingEntryObject
+    /// <summary>
+    /// EntryObject for mailing addresses used in the address book.
+    /// </summary>
+    public sealed class MailingEntryObject
     {
+        #region Properties
+
+        /// <summary>
+        /// Gets the mail address of this object.
+        /// </summary>
         public MailAddress Address { get; private set; }
+        /// <summary>
+        /// Gets the destined receipt type for mails sent to this address.
+        /// </summary>
         public ReceiptType Type { get; private set; }
 
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Parses a specified address and recipient type into a <see cref="MailingEntryObject"/>-instance.
+        /// </summary>
+        /// <param name="address">The e-mail address to parse.</param>
+        /// <param name="receiptType">The receipt type.</param>
+        /// <returns></returns>
         public static MailingEntryObject FromAddress(string address, string receiptType)
         {
             MailingEntryObject returnValue = new MailingEntryObject();
@@ -43,11 +64,30 @@ namespace AlarmWorkflow.Job.MailingJob
             return null;
         }
 
+        #endregion
+
+        #region Nested types
+
+        /// <summary>
+        /// Specifies the receipt type of a mail (To, Cc or Bcc).
+        /// </summary>
         public enum ReceiptType
         {
-            To,
+            /// <summary>
+            /// Direct addressee (To).
+            /// </summary>
+            To = 0,
+            /// <summary>
+            /// Carbon copy (Cc).
+            /// </summary>
             CC,
+            /// <summary>
+            /// Blind carbon copy (Bcc).
+            /// </summary>
             Bcc,
         }
+
+        #endregion
+
     }
 }
