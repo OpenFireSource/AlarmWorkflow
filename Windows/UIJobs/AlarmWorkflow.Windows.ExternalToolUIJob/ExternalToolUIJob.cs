86d52a273f70b9c2a61c93aecbc7ab6a517b281e
chrishensel <futurefwarrior@googlemail.com>
2012-12-08 16:52:12 +0100

    Created ExternalToolUIJob project

---
+++ Windows/UIJobs/AlarmWorkflow.Windows.ExternalToolUIJob/ExternalToolUIJob.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AlarmWorkflow.Shared.Core;
+using AlarmWorkflow.Windows.UIContracts.Extensibility;
+
+namespace AlarmWorkflow.Windows.ExternalToolUIJob
+{
+    [Export("ExternalToolUIJob", typeof(IUIJob))]
+    class ExternalToolUIJob : IUIJob
+    {
+        #region IUIJob Members
+
+        bool IUIJob.Initialize()
+        {
+            return true;
+        }
+
+        bool IUIJob.IsAsync
+        {
+            get { return true; }
+        }
+
+        void IUIJob.OnNewOperation(IOperationViewer operationViewer, Operation operation)
+        {
+
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        void IDisposable.Dispose()
+        {
+
+        }
+
+        #endregion
+    }
+}
