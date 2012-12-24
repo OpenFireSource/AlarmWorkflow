f05e1b993ed7081247726ebb0521d4682fe11fa7
chrishensel <futurefwarrior@googlemail.com>
2012-12-24 18:56:33 +0100

    Updated incorrect version info

--- Windows/UIWidget/AlarmWorkflow.Windows.UIWidgets.GoogleMaps/Properties/AssemblyInfo.cs
+++ Windows/UIWidget/AlarmWorkflow.Windows.UIWidgets.GoogleMaps/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -50,5 +50,5 @@ using System.Windows;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.1.0")]
+[assembly: AssemblyFileVersion("0.8.1.0")]
