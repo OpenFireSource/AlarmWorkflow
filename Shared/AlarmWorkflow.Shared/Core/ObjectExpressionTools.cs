979419849bee3348658a38f9624d8f75efb7e35a
chrishensel <futurefwarrior@googlemail.com>
2012-12-29 11:04:40 +0100

    Refactored setting properties from GenericParser

--- Shared/AlarmWorkflow.Shared/Core/ObjectExpressionTools.cs
+++ Shared/AlarmWorkflow.Shared/Core/ObjectExpressionTools.cs
@@ -32,6 +32,32 @@ namespace AlarmWorkflow.Shared.Core
         }
 
         /// <summary>
+        /// Traverses the property graph of an object and sets a certain property to a given value.
+        /// </summary>
+        /// <param name="graph">The object graph to use. Must not be null.</param>
+        /// <param name="expression">The expression of the property to set. Must not be empty.</param>
+        /// <param name="value">The value to set.</param>
+        /// <returns>Whether or not the value could be set. If this returns <c>false</c>, then the expression led to a nonexistent property.</returns>
+        public static bool TrySetValueFromExpression(object graph, string expression, object value)
+        {
+            Assertions.AssertNotNull(graph, "graph");
+            Assertions.AssertNotEmpty(expression, "expression");
+
+            PropertyInfo property = null;
+            object target = null;
+
+            bool success = GetPropertyFromExpression(graph, expression, false, out property, out target);
+            if (!success)
+            {
+                value = null;
+                return false;
+            }
+
+            property.SetValue(target, value, null);
+            return true;
+        }
+
+        /// <summary>
         /// Traverses the property graph of an object and returns the value of a certain property.
         /// </summary>
         /// <param name="graph">The object graph to use. Must not be null.</param>
@@ -57,7 +83,6 @@ namespace AlarmWorkflow.Shared.Core
         /// <param name="expression">The expression of the property to set. Must not be empty.</param>
         /// <param name="value">If the return value was <c>true</c>, then this contains the value of the property.</param>
         /// <returns>Whether or not the value could be retrieved. If this returns <c>false</c>, then the expression led to a nonexistent property.</returns>
-        /// <exception cref="System.MissingFieldException">A certain property in the expression was not found.</exception>
         public static bool TryGetValueFromExpression(object graph, string expression, out object value)
         {
             Assertions.AssertNotNull(graph, "graph");
@@ -77,7 +102,17 @@ namespace AlarmWorkflow.Shared.Core
             return true;
         }
 
-        private static bool GetPropertyFromExpression(object graph, string expression, bool throwOnMissing, out PropertyInfo property, out object target)
+        /// <summary>
+        /// Traverses the property graph of an object and returns the property at the end of the expression.
+        /// </summary>
+        /// <param name="graph">The object graph to use. Must not be null.</param>
+        /// <param name="expression">The expression of the property. Must not be empty.</param>
+        /// <param name="throwOnMissing">Whether or not to throw an exception if any property in the expression did not exist.</param>
+        /// <param name="property">If the return value is <c>true</c>, this parameter contains the property that was found.</param>
+        /// <param name="target">If the return value is <c>true</c>, this parameter contains the instance of the object on which the property was found.</param>
+        /// <returns>Whether or not the property could be found.</returns>
+        /// <exception cref="System.MissingFieldException">A certain property in the expression was not found.</exception>
+        public static bool GetPropertyFromExpression(object graph, string expression, bool throwOnMissing, out PropertyInfo property, out object target)
         {
             string[] tokens = expression.Split(new[] { '.' });
 
--- Shared/Parsers/AlarmWorkflow.Parser.GenericParser/Misc/Helpers.cs
+++ Shared/Parsers/AlarmWorkflow.Parser.GenericParser/Misc/Helpers.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Reflection;
 using AlarmWorkflow.Shared.Core;
-using AlarmWorkflow.Shared.Diagnostics;
 
 namespace AlarmWorkflow.Parser.GenericParser.Misc
 {
     static class Helpers
     {
+        internal static bool SetValueFromExpression(object graph, string expression, string value)
+        {
+            object target = null;
+            PropertyInfo property = null;
+            if (ObjectExpressionTools.GetPropertyFromExpression(graph, expression, false, out property, out target))
+            {
+                return ObjectExpressionTools.TrySetValueFromExpression(graph, expression, StringToObjectSimple(property.PropertyType, value));
+            }
+            return false;
+        }
+
         private static object StringToObjectSimple(Type targetType, string value)
         {
             object realValue = null;
@@ -25,44 +35,6 @@ namespace AlarmWorkflow.Parser.GenericParser.Misc
                     break;
             }
             return realValue;
-        }
-
-        internal static bool SetValueFromExpression(object graph, string expression, string value)
-        {
-            Assertions.AssertNotNull(graph, "graph");
-            Assertions.AssertNotEmpty(expression, "expression");
-
-            string[] tokens = expression.Split(new[] { '.' });
-
-            // Find property
-            object currentObject = graph;
-            PropertyInfo property = null;
-            for (int i = 0; i < tokens.Length; i++)
-            {
-                string propertyName = tokens[i];
-
-                property = currentObject.GetType().GetProperty(propertyName);
-                if (property == null)
-                {
-                    Logger.Instance.LogFormat(LogType.Warning, typeof(GenericParser), "Property with name '{0}' was not found in object type '{1}' (expression was '{2}').", propertyName, currentObject.GetType().Name, expression);
-                    break;
-                }
-
-                if (i < tokens.Length - 1)
-                {
-                    // Next iteration... step down one hierarchy level
-                    currentObject = property.GetValue(currentObject, null);
-                }
-            }
-
-            if (property != null)
-            {
-                property.SetValue(currentObject, StringToObjectSimple(property.PropertyType, value), null);
-                return true;
-            }
-
-            // Property not found
-            return false;
         }
     }
 }
