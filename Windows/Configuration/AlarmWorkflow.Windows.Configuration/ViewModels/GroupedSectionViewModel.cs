f79de0cc1dd73e805077a5d653b1d9a6b79901d9
chrishensel <futurefwarrior@googlemail.com>
2013-01-24 18:05:13 +0100

    Added missing VM

---
+++ Windows/Configuration/AlarmWorkflow.Windows.Configuration/ViewModels/GroupedSectionViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using AlarmWorkflow.Windows.UIContracts.ViewModels;
+
+namespace AlarmWorkflow.Windows.Configuration.ViewModels
+{
+    class GroupedSectionViewModel : ViewModelBase
+    {
+        #region Properties
+
+        public List<GroupedSectionViewModel> Children { get; private set; }
+        public SectionViewModel Section { get; private set; }
+
+        public bool IsSelected { get; set; }
+        public string Identifier { get; set; }
+        public string Header { get; set; }
+        public int Order { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        internal GroupedSectionViewModel(SectionViewModel section)
+        {
+            Children = new List<GroupedSectionViewModel>();
+
+            if (section != null)
+            {
+                Section = section;
+
+                Identifier = Section.Identifier;
+                Header = Section.DisplayText;
+                Order = section.Order;
+            }
+        }
+
+        #endregion
+    }
+}
