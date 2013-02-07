73ad3de82e478feb3c2022942bca920aa45c8660
Florian <florian.olching@gmail.com>
2013-02-07 14:15:21 +0100

    Fixed MySQL Script

--- Shared/Jobs/AlarmWorkflow.Job.MySqlDatabaseJob/Schema/alarmworkflow.sql
+++ Shared/Jobs/AlarmWorkflow.Job.MySqlDatabaseJob/Schema/alarmworkflow.sql
@@ -1,29 +1,13 @@
---
--- Datenbank: `alarmworkflow`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE SCHEMA `alarmworkflow`;
+DROP DATABASE IF EXISTS `alarmworkflow`;
+CREATE DATABASE IF NOT EXISTS `alarmworkflow` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `alarmworkflow`;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `accessright`
---
-
-CREATE TABLE IF NOT EXISTS `accessright` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(100) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `Name_UNIQUE` (`Name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `operation`
---
-
+DROP TABLE IF EXISTS `operation`;
 CREATE TABLE IF NOT EXISTS `operation` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `IsAcknowledged` bit(1) NOT NULL DEFAULT b'0',
@@ -31,14 +15,9 @@ CREATE TABLE IF NOT EXISTS `operation` (
   `OperationId` char(36) NOT NULL,
   `Serialized` longblob NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `tb_einsatz`
---
-
+DROP TABLE IF EXISTS `tb_einsatz`;
 CREATE TABLE IF NOT EXISTS `tb_einsatz` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `Einsatznr` varchar(255) DEFAULT NULL,
@@ -57,51 +36,8 @@ CREATE TABLE IF NOT EXISTS `tb_einsatz` (
   `Faxtime` varchar(255) DEFAULT NULL,
   `Stichwort` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(100) NOT NULL,
-  `Password` blob,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `Name_UNIQUE` (`Name`),
-  KEY `FK_User_Person_JI_idx` (`Person_Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user_accessright`
---
-
-CREATE TABLE IF NOT EXISTS `user_accessright` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `User_Id` int(11) NOT NULL,
-  `AccessRight_Id` int(11) NOT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `FK_User_AccessRight_User_Id_idx` (`User_Id`),
-  KEY `FK_User_AccessRight_AccessRight_Id_idx` (`AccessRight_Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `FK_User_Person_Id` FOREIGN KEY (`Person_Id`) REFERENCES `person` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints der Tabelle `user_accessright`
---
-ALTER TABLE `user_accessright`
-  ADD CONSTRAINT `FK_User_AccessRight_User_Id` FOREIGN KEY (`User_Id`) REFERENCES `user` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `FK_User_AccessRight_AccessRight_Id` FOREIGN KEY (`AccessRight_Id`) REFERENCES `accessright` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
