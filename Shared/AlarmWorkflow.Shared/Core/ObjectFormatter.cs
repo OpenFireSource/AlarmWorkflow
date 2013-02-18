604ec111f4e067e7aef5c63bccd63913e920da6c
chrishensel <futurefwarrior@googlemail.com>
2013-02-18 22:29:30 +0100

    Updated ObjectFormatter
    - Allows more control over formatting process
    - Made default formatting options more relaxed

--- Shared/AlarmWorkflow.Shared/Core/ObjectFormatter.cs
+++ Shared/AlarmWorkflow.Shared/Core/ObjectFormatter.cs
@@ -9,6 +9,15 @@ namespace AlarmWorkflow.Shared.Core
     /// </summary>
     public static class ObjectFormatter
     {
+        #region Constants
+
+        /// <summary>
+        /// Represents the string that is inserted if a value is null. Only used if the corresponding option is specified.
+        /// </summary>
+        private const string NullValueString = "[?]";
+
+        #endregion
+
         #region Methods
 
         /// <summary>
@@ -19,18 +28,41 @@ namespace AlarmWorkflow.Shared.Core
         /// <returns>The formatted string.</returns>
         public static string ToString(object graph, string format)
         {
+            return ToString(graph, format, ObjectFormatterOptions.Default);
+        }
+
+        /// <summary>
+        /// Parses a string that tells how to format an object using macros within curly braces.
+        /// </summary>
+        /// <param name="graph">The object graph to use. Must not be null.</param>
+        /// <param name="format">The format string, using the property values in curly braces, like {Property}. Must not be empty.</param>
+        /// <param name="options">Controls the formatting process.</param>
+        /// <returns>The formatted string.</returns>
+        public static string ToString(object graph, string format, ObjectFormatterOptions options)
+        {
             Assertions.AssertNotNull(graph, "graph");
             Assertions.AssertNotEmpty(format, "format");
 
+            string nullValueString = "";
+            if (options.HasFlag(ObjectFormatterOptions.InsertQuestionMarksForNullValues))
+            {
+                nullValueString = NullValueString;
+            }
+
             StringBuilder sb = new StringBuilder(format);
-            // Replace common control chars
-            sb.Replace("\n", Environment.NewLine);
+
+            // Check whether or not to remove the newlines
+            if (options.HasFlag(ObjectFormatterOptions.RemoveNewlines))
+            {
+                sb.Replace("\n", " ");
+                sb.Replace(Environment.NewLine, " ");
+            }
 
             foreach (string macro in GetMacros(format))
             {
                 string expression = macro.Substring(1, macro.Length - 2);
 
-                string propertyValue = "[?]";
+                string propertyValue = nullValueString;
                 object rawValue = null;
 
                 bool propertyFound = ObjectExpressionTools.TryGetValueFromExpression(graph, expression, out rawValue);
---
+++ Shared/AlarmWorkflow.Shared/Core/ObjectFormatterOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AlarmWorkflow.Shared.Core
+{
+    /// <summary>
+    /// Specifies the options that can be used to alter the way the <see cref="T:ObjectFormatter" /> formats an object.
+    /// </summary>
+    [Flags()]
+    public enum ObjectFormatterOptions
+    {
+        /// <summary>
+        /// Uses no options. This is the default value.
+        /// </summary>
+        Default = 0,
+        /// <summary>
+        /// Removes newline characters from the format string, resulting in one long line.
+        /// </summary>
+        RemoveNewlines = 1,
+        /// <summary>
+        /// Inserts a question mark '[?]' for any value that is <c>null</c>.
+        /// If this option is not specified, nothing is inserted (empty).
+        /// </summary>
+        InsertQuestionMarksForNullValues = 2,
+    }
+}
