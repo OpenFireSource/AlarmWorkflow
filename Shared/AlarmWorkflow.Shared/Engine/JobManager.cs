737f249904d91a93b59179b3f35f2f3701f96260
Florian <florian.olching@gmail.com>
2013-03-02 18:34:14 +0100

    Working with format params

--- Shared/AlarmWorkflow.Shared/Engine/JobManager.cs
+++ Shared/AlarmWorkflow.Shared/Engine/JobManager.cs
@@ -89,7 +89,7 @@ namespace AlarmWorkflow.Shared.Engine
         {
             if (job.IsAsync)
             {
-                Logger.Instance.LogFormat(LogType.Info, this, "Executing asnyc job " + job.GetType().Name);
+                Logger.Instance.LogFormat(LogType.Info, this, "Executing asnyc job {0}.", job.GetType().Name);
                 ThreadPool.QueueUserWorkItem(o =>
                 {
                     RunJobCore(context, operation, job);
@@ -97,7 +97,7 @@ namespace AlarmWorkflow.Shared.Engine
             }
             else
             {
-                Logger.Instance.LogFormat(LogType.Info, this, "Executing asnyc job " + job.GetType().Name);
+                Logger.Instance.LogFormat(LogType.Info, this, "Executing asnyc job {0}." , job.GetType().Name);
                 RunJobCore(context, operation, job);
             }
         }
