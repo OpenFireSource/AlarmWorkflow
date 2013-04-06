ea730c5239c3aab7d36060bf7f480f7e4b400882
chrishensel <futurefwarrior@googlemail.com>
2013-04-06 22:22:19 +0200

    Added tab to ConfEd displaying error reports
    First version. Lists all .erx-files present in the error
    reports-directory. Also allows for manually drag 'n dropping .erx-files
    into the listbox to display them.
    
    Also updated AssemblyInfo.cs'.

---
+++ Windows/Configuration/AlarmWorkflow.Windows.Configuration/Views/ErrorReportsView.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using AlarmWorkflow.Shared.Diagnostics.Reports;
+using AlarmWorkflow.Windows.Configuration.ViewModels;
+
+namespace AlarmWorkflow.Windows.Configuration.Views
+{
+    /// <summary>
+    /// Interaction logic for ErrorReportsView.xaml
+    /// </summary>
+    public partial class ErrorReportsView : UserControl
+    {
+        #region Fields
+
+        private ErrorReportsViewModel _viewModel;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorReportsView"/> class.
+        /// </summary>
+        public ErrorReportsView()
+        {
+            InitializeComponent();
+
+            _viewModel = new ErrorReportsViewModel();
+            this.DataContext = _viewModel;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void lsvReports_Drop(object sender, System.Windows.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                HandleDroppedFile(e.Data.GetData(DataFormats.FileDrop));
+            }
+        }
+
+        private void HandleDroppedFile(object data)
+        {
+            _viewModel.ErrorReports.Clear();
+            foreach (string fileName in (string[])data)
+            {
+                try
+                {
+                    ErrorReport report = ErrorReport.Deserialize(File.ReadAllText(fileName));
+
+                    _viewModel.AddSingleErrorReport(report);
+                }
+                catch (System.Exception)
+                {
+                    // TODO: Handling (message box).
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
