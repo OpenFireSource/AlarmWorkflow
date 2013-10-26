5aaf06835bc15148ff3a69d74e9ca51a23e9ca8c
chrishensel <futurefwarrior@googlemail.com>
2013-10-26 18:15:20 +0200

    Added script file to build project in Release mode
    To be used when building official release files.

---
+++ BuildWindows.Release.bat
@@ -0,0 +1,20 @@
+@echo off
+echo -------------------------------------------------
+echo Build Shared...
+msbuild Shared\AlarmWorkflow.Shared.sln /p:Configuration=Release /verbosity:minimal
+msbuild Shared\AlarmWorkflow.Shared.AlarmSources.sln /p:Configuration=Release /verbosity:minimal
+msbuild Shared\AlarmWorkflow.Shared.Parser.sln /p:Configuration=Release /verbosity:minimal
+msbuild Shared\AlarmWorkflow.Shared.Jobs.sln /p:Configuration=Release /verbosity:minimal
+
+echo -------------------------------------------------
+echo Build Windows-specific stuff...
+msbuild Service\AlarmWorkflow.Service.sln /p:Configuration=Release /verbosity:minimal
+
+msbuild Windows\AlarmWorkflow.Windows.sln /p:Configuration=Release /verbosity:minimal
+msbuild Windows\AlarmWorkflow.Windows.Configuration.sln /p:Configuration=Release /verbosity:minimal
+msbuild Windows\AlarmWorkflow.Windows.UIJobs.sln /p:Configuration=Release /verbosity:minimal
+msbuild Windows\AlarmWorkflow.Windows.OperationViewer.sln /p:Configuration=Release /verbosity:minimal
+
+msbuild Windows\AlarmWorkflow.Windows.UIWidget.sln /p:Configuration=Release /verbosity:minimal
+
+pause
