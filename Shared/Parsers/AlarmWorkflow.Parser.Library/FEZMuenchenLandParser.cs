9b62842065477b62fde56e1e0b7d57aef2c7e290
Florian <florian.olching@gmail.com>
2014-01-15 18:09:15 +0100

    [Parser] Refractoring FEZMuenchenLandParser Parser
    Now reads timestamp, 'Einsatzort Abschnitt', resources and is in 'standard-layout'.

--- Shared/Parsers/AlarmWorkflow.Parser.Library/FEZMuenchenLandParser.cs
+++ Shared/Parsers/AlarmWorkflow.Parser.Library/FEZMuenchenLandParser.cs
@@ -14,83 +14,208 @@
 // along with AlarmWorkflow.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Linq;
 using AlarmWorkflow.Shared.Core;
-using AlarmWorkflow.Shared.Diagnostics;
 using AlarmWorkflow.Shared.Extensibility;
 
 namespace AlarmWorkflow.Parser.Library
 {
-    [Export("FEZMuenchenLandParser", typeof(IParser))]
-    sealed class FEZMuenchenLandParser : IParser
+    [Export("FezMuenchenLandParser", typeof(IParser))]
+    class FezMuenchenLandParser : IParser
     {
+        #region Fields
 
-        #region IFaxParser Members
+        private readonly string[] _keywords =
+        {
+            "EINSATZNR","MITTEILER","EINSATZORT","STRAßE","ABSCHNITT","KREUZUNG",
+            "ORTSTEIL/ORT","OBJEKT","EINSATZPLAN","MELDEBILD"
+            ,"HINWEIS","GEFORDERTE EINSATZMITTEL","(ALARMSCHREIBEN ENDE)"
+        };
+
+        #endregion
+
+        #region IParser Members
 
         Operation IParser.Parse(string[] lines)
         {
             Operation operation = new Operation();
-
+            CurrentSection section = CurrentSection.AAnfang;
             lines = Utilities.Trim(lines);
-
-            foreach (string line in lines)
+            foreach (var line in lines)
             {
-                try
+                string keyword;
+                if (GetKeyword(line, out keyword))
                 {
-                    string msg;
-                    string prefix;
-                    int x = line.IndexOf(':');
-                    if (x == -1)
+                    switch (keyword.Trim())
                     {
-                        continue;
-                    }
-
-                    prefix = line.Substring(0, x);
-                    msg = line.Substring(x + 1).Trim();
-
-                    prefix = prefix.Trim().ToUpperInvariant();
-                    switch (prefix)
-                    {
-                        case "EINSATZNR":
-                            operation.OperationNumber = msg;
-                            break;
-                        case "MITTEILER":
-                            operation.Messenger = msg;
-                            break;
-                        case "EINSATZORT":
-                            operation.Einsatzort.Location = msg;
-                            break;
-                        case "STRAßE":
-                        case "STRABE":
-                            operation.Einsatzort.Street = msg;
-                            break;
-                        case "KREUZUNG":
-                            operation.Einsatzort.Intersection = msg;
-                            break;
-                        case "ORTSTEIL/ORT":
-                            operation.Einsatzort.City = msg;
-                            break;
-                        case "OBJEKT":
-                        case "9BJEKT":
-                            operation.Einsatzort.Property = msg;
-                            break;
-                        case "MELDEBILD":
-                            operation.Picture = msg;
-                            break;
-                        case "HINWEIS":
-                            operation.Comment = msg;
-                            break;
-                        case "EINSATZPLAN":
-                            operation.OperationPlan = msg;
-                            break;
+                        case "EINSATZNR": { section = CurrentSection.BeNr; break; }
+                        case "MITTEILER": { section = CurrentSection.CMitteiler; break; }
+                        case "EINSATZORT": { section = CurrentSection.DEinsatzort; break; }
+                        case "STRAßE": { section = CurrentSection.EStraße; break; }
+                        case "ABSCHNITT": { section = CurrentSection.FAbschnitt; break; }
+                        case "KREUZUNG": { section = CurrentSection.GKreuzung; break; }
+                        case "ORTSTEIL/ORT": { section = CurrentSection.HOrt; break; }
+                        case "OBJEKT": { section = CurrentSection.JObjekt; break; }
+                        case "EINSATZPLAN": { section = CurrentSection.KEinsatzplan; break; }
+                        case "MELDEBILD": { section = CurrentSection.LMeldebild; break; }
+                        case "HINWEIS": { section = CurrentSection.MHinweis; break; }
+                        case "GEFORDERTE EINSATZMITTEL": { section = CurrentSection.NEinsatzmittel; break; }
+                        case "(ALARMSCHREIBEN ENDE)": { section = CurrentSection.OEnde; break; }
                     }
                 }
-                catch (Exception ex)
+
+
+                switch (section)
                 {
-                    Logger.Instance.LogException(this, ex);
+                    case CurrentSection.BeNr:
+                        int indexOf = line.IndexOf("ALARM", StringComparison.InvariantCultureIgnoreCase);
+                        if (indexOf == -1)
+                        {
+                            operation.OperationNumber = GetMessageText(line, keyword);
+                            break;
+                        }
+                        operation.OperationNumber = GetMessageText(line.Substring(0, indexOf), keyword);
+                        keyword = "ALARM";
+                        try
+                        {
+                            operation.Timestamp = DateTime.Parse(GetMessageText(line.Substring(indexOf), keyword));
+                        }
+                        catch (FormatException)
+                        {
+                            operation.Timestamp = DateTime.Now;
+                        }
+                        break;
+                    case CurrentSection.CMitteiler:
+                        operation.Messenger = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.DEinsatzort:
+                        operation.Einsatzort.Location = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.EStraße:
+                        operation.Einsatzort.Street = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.FAbschnitt:
+                        operation.CustomData["Einsatzort Abschnitt"] = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.GKreuzung:
+                        operation.Einsatzort.Intersection = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.HOrt:
+                        operation.Einsatzort.City = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.JObjekt:
+                        operation.Einsatzort.Property = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.KEinsatzplan:
+                        operation.OperationPlan = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.LMeldebild:
+                        operation.Picture = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.MHinweis:
+                        operation.Comment = GetMessageText(line, keyword);
+                        break;
+                    case CurrentSection.NEinsatzmittel:
+                        if (line.StartsWith("Geforderte Einsatzmittel", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            break;
+                        }
+                        OperationResource resource = new OperationResource();
+                        if (line.Contains('('))
+                        {
+                            string tool = line.Substring(line.IndexOf("(", StringComparison.Ordinal) + 1);
+                            tool = tool.Length >= 2 ? tool.Substring(0, tool.Length - 2).Trim() : String.Empty;
+                            string unit = line.Substring(0, line.IndexOf("(", StringComparison.Ordinal));
+                            resource.FullName = unit;
+                            resource.RequestedEquipment.Add(tool);
+                            operation.Resources.Add(resource);
+
+                        }
+                        else
+                        {
+                            operation.Resources.Add(new OperationResource() { FullName = line });
+                        }
+                        break;
+
+                    case CurrentSection.OEnde:
+                        break;
+
                 }
             }
 
             return operation;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private bool GetKeyword(string line, out string keyword)
+        {
+            line = line.ToUpperInvariant();
+            foreach (string kwd in _keywords.Where(kwd => line.ToLowerInvariant().StartsWith(kwd.ToLowerInvariant())))
+            {
+                keyword = kwd;
+                return true;
+            }
+            keyword = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the message text, which is the line text but excluding the keyword/prefix and a possible colon.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="prefix">The prefix that is to be removed (optional).</param>
+        /// <returns></returns>
+        private string GetMessageText(string line, string prefix)
+        {
+            if (prefix == null)
+            {
+                prefix = "";
+            }
+
+            if (prefix.Length > 0)
+            {
+                line = line.Remove(0, prefix.Length);
+            }
+            else
+            {
+                int colonIndex = line.IndexOf(':');
+                if (colonIndex != -1)
+                {
+                    line = line.Remove(0, colonIndex + 1);
+                }
+            }
+
+            if (line.StartsWith(":"))
+            {
+                line = line.Remove(0, 1);
+            }
+            line = line.Trim();
+            return line;
+        }
+
+        #endregion
+
+        #region Nested types
+
+        private enum CurrentSection
+        {
+            AAnfang,
+            BeNr,
+            CMitteiler,
+            DEinsatzort,
+            EStraße,
+            FAbschnitt,
+            GKreuzung,
+            HOrt,
+            JObjekt,
+            KEinsatzplan,
+            LMeldebild,
+            MHinweis,
+            NEinsatzmittel,
+            OEnde
         }
 
         #endregion
