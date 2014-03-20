ad3dce2d61b32378eddc508cbdce857464b966a6
chrishensel <futurefwarrior@googlemail.com>
2014-03-20 19:53:49 +0100

    Added file transfer service
    Added basic file transfer service (internal only right now), which has the
    purpose to allow controlled (read-only) access to resource files on the
    server.
    This will be used later within the clients to access the icon files of the
    resources (stored in the EMK data).

---
+++ BackendServices/AlarmWorkflow.BackendService.FileTransfer/FileTransferServiceInternal.cs
@@ -0,0 +1,107 @@
+﻿// This file is part of AlarmWorkflow.
+// 
+// AlarmWorkflow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AlarmWorkflow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with AlarmWorkflow.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using AlarmWorkflow.Backend.ServiceContracts.Core;
+using AlarmWorkflow.BackendService.FileTransferContracts;
+using AlarmWorkflow.Shared.Core;
+
+/* 
+ * Attention for extending this service!
+ * 
+ * This service shall never compromise the system the service is running on!
+ * For this reason, access to any file outside of the AlarmWorkflow's local directory
+ * shall be restricted! The same shall apply to write access - if any.
+ * 
+ * Also never expose the absolute file paths through an error message.
+ * Always work with the relative paths.
+ * 
+ * This service is not the best idea, security-wise. But sometimes it's just necessary
+ * to provide some data to clients.
+ * 
+ * Like for example, the EMK, allows to register icons for the resources. We may want to
+ * provide those icons to clients like the website or the UI, or even an app.
+ * 
+ * Bottom line: Please be extremely careful with extending this service!
+ * 
+ */
+
+namespace AlarmWorkflow.BackendService.FileTransfer
+{
+    class FileTransferServiceInternal : InternalServiceBase, IFileTransferServiceInternal
+    {
+        #region Methods
+
+        private static void AssertPathIsNotRootedAndExists(string path)
+        {
+            Assertions.AssertNotEmpty(path, "path");
+
+            if (Path.IsPathRooted(path))
+            {
+                throw new IOException(Properties.Resources.FilePathIsRootError);
+            }
+
+            string localPath = GetLocalPathForRelative(path);
+            if (!File.Exists(localPath))
+            {
+                throw new FileNotFoundException(Properties.Resources.FilePathNotFoundError, path);
+            }
+        }
+
+        private static string GetLocalPathForRelative(string path)
+        {
+            return Utilities.GetLocalAppDataFolderFileName(path);
+        }
+
+        #endregion
+
+        #region IFileTransferServiceInternal Members
+
+        string IFileTransferServiceInternal.GetFileChecksum(string path)
+        {
+            AssertPathIsNotRootedAndExists(path);
+
+            using (MD5 md5 = MD5.Create())
+            {
+                using (FileStream stream = new FileStream(GetLocalPathForRelative(path), FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    byte[] result = md5.ComputeHash(stream);
+
+                    StringBuilder strBuilder = new StringBuilder();
+                    for (int i = 0; i < result.Length; i++)
+                    {
+                        strBuilder.Append(result[i].ToString("x2"));
+                    }
+
+                    return strBuilder.ToString();
+                }
+            }
+        }
+
+        Stream IFileTransferServiceInternal.GetFileStream(string path)
+        {
+            AssertPathIsNotRootedAndExists(path);
+
+            using (FileStream stream = new FileStream(GetLocalPathForRelative(path), FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return stream;
+            }
+        }
+
+        #endregion
+    }
+}
