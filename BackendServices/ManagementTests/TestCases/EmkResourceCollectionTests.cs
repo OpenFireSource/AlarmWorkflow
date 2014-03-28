1ca98cf58a7e045abeca578c60261092b7c8c8f4
chrishensel <futurefwarrior@googlemail.com>
2014-03-28 19:13:24 +0100

    Added new test for verification of "IsActive"

--- BackendServices/ManagementTests/TestCases/EmkResourceCollectionTests.cs
+++ BackendServices/ManagementTests/TestCases/EmkResourceCollectionTests.cs
@@ -38,5 +38,21 @@ namespace AlarmWorkflow.Test.Management.TestCases
 
             Assert.IsFalse(collection.ContainsMatch(opRes));
         }
+
+        [TestMethod]
+        public void ContainsMatch_WithInactiveOnes_Negative_Test()
+        {
+            EmkResourceCollection collection = new EmkResourceCollection();
+            collection.Add(new EmkResource() { SiteAlias = "ABC", ResourceAlias = "77/7" });
+            collection.Add(new EmkResource() { SiteAlias = "ABC", ResourceAlias = "88/8", IsActive = false });
+            collection.Add(new EmkResource() { SiteAlias = "ABC", ResourceAlias = "99/9" });
+
+            OperationResource opRes = new OperationResource()
+            {
+                FullName = "1.2.3 ABC 88/8 LF",
+            };
+
+            Assert.IsFalse(collection.ContainsMatch(opRes));
+        }
     }
 }
