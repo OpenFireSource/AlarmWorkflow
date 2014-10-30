9a0cc5d2f6bf6d2d1db33f7cf631035d20bf21d4
Florian <florian.olching@gmail.com>
2014-10-30 16:50:37 +0100

    Made more settings dynamic (faxalarmsource)

--- AlarmSources/Fax/FaxConfiguration.cs
+++ AlarmSources/Fax/FaxConfiguration.cs
@@ -15,15 +15,24 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using AlarmWorkflow.BackendService.SettingsContracts;
 using AlarmWorkflow.Shared;
 using AlarmWorkflow.Shared.Core;
+using AlarmWorkflow.Shared.Settings;
 using AlarmWorkflow.Shared.Specialized;
 
 namespace AlarmWorkflow.AlarmSource.Fax
 {
-    internal sealed class FaxConfiguration : DisposableObject
+    internal sealed class FaxConfiguration : DisposableObject, INotifyPropertyChanged
     {
+        #region Constants
+
+        private const string Identifier = "FaxAlarmSource";
+
+        #endregion
+
         #region Fields
 
         private ISettingsServiceInternal _settings;
@@ -32,11 +41,30 @@ namespace AlarmWorkflow.AlarmSource.Fax
 
         #region Properties
 
-        internal string FaxPath { get; private set; }
-        internal string ArchivePath { get; private set; }
-        internal string AnalysisPath { get; private set; }
-        internal string OCRSoftwarePath { get; private set; }
-        internal string AlarmFaxParserAlias { get; private set; }
+        internal string FaxPath
+        {
+            get { return _settings.GetSetting(FaxSettingKeys.FaxPath).GetValue<string>(); }
+        }
+
+        internal string ArchivePath
+        {
+            get { return _settings.GetSetting(FaxSettingKeys.ArchivePath).GetValue<string>(); }
+        }
+
+        internal string AnalysisPath
+        {
+            get { return _settings.GetSetting(FaxSettingKeys.AnalysisPath).GetValue<string>(); }
+        }
+
+        internal string OCRSoftwarePath
+        {
+            get { return _settings.GetSetting(FaxSettingKeys.OcrPath).GetValue<string>(); }
+        }
+
+        internal string AlarmFaxParserAlias
+        {
+            get { return _settings.GetSetting(FaxSettingKeys.AlarmFaxParserAlias).GetValue<string>(); }
+        }
 
         internal IEnumerable<string> FaxBlacklist
         {
@@ -64,13 +92,37 @@ namespace AlarmWorkflow.AlarmSource.Fax
         public FaxConfiguration(IServiceProvider serviceProvider)
         {
             _settings = serviceProvider.GetService<ISettingsServiceInternal>();
+            _settings.SettingChanged += _settings_SettingChanged;
+        }
 
-            this.FaxPath = _settings.GetSetting("FaxAlarmSource", "FaxPath").GetValue<string>();
-            this.ArchivePath = _settings.GetSetting("FaxAlarmSource", "ArchivePath").GetValue<string>();
-            this.AnalysisPath = _settings.GetSetting("FaxAlarmSource", "AnalysisPath").GetValue<string>();
-            this.AlarmFaxParserAlias = _settings.GetSetting("FaxAlarmSource", "AlarmfaxParser").GetValue<string>();
+        #endregion
 
-            this.OCRSoftwarePath = _settings.GetSetting("FaxAlarmSource", "OCR.Path").GetValue<string>();
+        #region Event-Handler
+
+        private void _settings_SettingChanged(object sender, SettingChangedEventArgs e)
+        {
+            List<string> changedKeys = new List<string>();
+            foreach (SettingKey key in e.Keys)
+            {
+                if (key.Equals(FaxSettingKeys.AlarmFaxParserAlias))
+                {
+                    changedKeys.Add("AlarmfaxParser");
+                }
+                else if (key.Equals(FaxSettingKeys.OcrPath))
+                {
+                    changedKeys.Add("OCR.Path");
+                }
+                else if ((key.Equals(FaxSettingKeys.FaxPath) || key.Equals(FaxSettingKeys.AnalysisPath) || key.Equals(FaxSettingKeys.ArchivePath)) && !changedKeys.Contains("FaxPaths"))
+                {
+                    changedKeys.Add("FaxPaths");
+                }
+            }
+
+            foreach (string key in changedKeys)
+            {
+                OnPropertyChanged(key);
+            }
+
         }
 
         #endregion
@@ -92,10 +144,25 @@ namespace AlarmWorkflow.AlarmSource.Fax
         /// </summary>
         protected override void DisposeCore()
         {
+            _settings.SettingChanged -= _settings_SettingChanged;
             _settings = null;
         }
 
         #endregion
+        
+        #region INotifyPropertyChanged Members
+        
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        
+        #endregion
     }
 }
