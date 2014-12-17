0f23ec8d6bcafe96582506b0ff91d472afcc6507
chrishensel <futurefwarrior@googlemail.com>
2014-12-17 18:02:02 +0100

    Moved Website into separate project
    The reason for this is that the website is more or less its own sub-project and gets updated irregularly with no relation to the "core" AlarmWorkflow libraries, so outsourcing seemed a good idea.
    
    The new project can be found at https://github.com/OpenFireSource/AlarmWorkflow-Website.

--- BuildWebsite.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo -------------------------------------------------
-echo Build Website(s)...
-msbuild Website\Website.sln /p:Configuration=Debug /verbosity:minimal
-
-pause
--- Website/Website/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AlarmWorkflow.Website.Reports.MvcApplication" Language="C#" %>
