1a107a9ee86bfd45a007fec5c31c8e2cdd074ec3
chrishensel <futurefwarrior@googlemail.com>
2015-04-18 14:29:23 +0200

    Several build script fixes
    - Do not require modifying PATH variable anymore
    - Also build Tools solution in RELEASE script

--- BuildWindows.Release.bat
+++ BuildWindows.Release.bat
@@ -1,24 +1,34 @@
 @echo off
+rem -------------------------------------------------
+rem AlarmWorkflow build script (RELEASE)
+rem 
+rem Please change "Framework64" to "Framework" in the following path
+rem if you're running this script on a x86 or 32-bit OS.
+rem -------------------------------------------------
+SET build=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe
+
 echo -------------------------------------------------
 echo Build submodules...
-msbuild externals\S22.Imap\S22.Imap.sln /p:Configuration=Release /verbosity:minimal
+%build% externals\S22.Imap\S22.Imap.sln /p:Configuration=Release /verbosity:minimal
 
 echo -------------------------------------------------
 echo Build Shared...
-msbuild Shared\Shared.sln /p:Configuration=Release /verbosity:minimal
+%build% Shared\Shared.sln /p:Configuration=Release /verbosity:minimal
 
 echo -------------------------------------------------
 echo Build Windows-specific stuff...
-msbuild Backend\Backend.sln /p:Configuration=Release /verbosity:minimal
-msbuild BackendServices\BackendServices.sln /p:Configuration=Release /verbosity:minimal
+%build% Backend\Backend.sln /p:Configuration=Release /verbosity:minimal
+%build% BackendServices\BackendServices.sln /p:Configuration=Release /verbosity:minimal
 
-msbuild AlarmSources\AlarmSources.sln /p:Configuration=Release /verbosity:minimal
+%build% AlarmSources\AlarmSources.sln /p:Configuration=Release /verbosity:minimal
 
-msbuild Windows\Windows.sln /p:Configuration=Release /verbosity:minimal
-msbuild WindowsUIWidgets\WindowsUIWidgets.sln /p:Configuration=Release /verbosity:minimal
-msbuild Configuration\Configuration.sln /p:Configuration=Release /verbosity:minimal
+%build% Windows\Windows.sln /p:Configuration=Release /verbosity:minimal
+%build% WindowsUIWidgets\WindowsUIWidgets.sln /p:Configuration=Release /verbosity:minimal
+%build% Configuration\Configuration.sln /p:Configuration=Release /verbosity:minimal
 
-msbuild Jobs\Engine\EngineJobs.sln /p:Configuration=Release /verbosity:minimal
-msbuild Jobs\WindowsUI\WindowsUIJobs.sln /p:Configuration=Release /verbosity:minimal
+%build% Jobs\Engine\EngineJobs.sln /p:Configuration=Release /verbosity:minimal
+%build% Jobs\WindowsUI\WindowsUIJobs.sln /p:Configuration=Release /verbosity:minimal
+
+%build% Tools\Tools.sln /p:Configuration=Release /verbosity:minimal
 
 pause
