7a232351117a1d41b2bffee7f18c75e0f04b2ac5
Stieglitz <stieglit@hm.edu>
2015-10-05 17:55:01 +0200

    Add AlarmSourceNetwork
    Add a alarm source from a network interface. For sending a alarm from a
    extern program like a dummy or FMS32

---
+++ AlarmSources/Network/Server/AlarmServer.cs
@@ -0,0 +1,101 @@
+﻿// This file is part of AlarmWorkflow.
+// 
+// AlarmWorkflow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AlarmWorkflow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with AlarmWorkflow.  If not, see <http://www.gnu.org/licenses/>.
+
+using AlarmWorkflow.Shared.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmWorkflow.AlarmSource.Network.Server
+{
+    class AlarmServer
+    {
+
+        #region Properties
+        
+        public bool IsRunning { get; private set; }
+
+        #endregion
+
+        #region Fields
+
+        private readonly NetworkAlarmSource _parent;
+        private TcpListener _serverSocket;
+        private TcpClient _clientSocket;
+        private int _listenPort;
+		
+        #endregion
+
+        #region Constructors
+
+        internal AlarmServer(NetworkAlarmSource parent, int listenPort)
+        {
+            _parent = parent;
+            _listenPort = listenPort;
+            _serverSocket = new TcpListener(IPAddress.Any, _listenPort);
+        }
+
+        #endregion
+
+        #region Methods
+
+        internal void Start()
+        {
+            try
+            {
+                _serverSocket.Start();
+                IsRunning = true;
+
+                Logger.Instance.LogFormat(LogType.Trace, this, Properties.Resources.NetworkServerStarted, _listenPort);
+            }
+            catch (SocketException ex)
+            {
+                Logger.Instance.LogFormat(LogType.Error, this, Properties.Resources.NetworkSocketError, ex);
+                Logger.Instance.LogException(this, ex);
+            }
+
+            while (true)
+            {
+                try
+                {
+                    _clientSocket = _serverSocket.AcceptTcpClient();
+                    HandleAlarmClient.StartClient(_clientSocket, _parent);
+                }
+                catch(Exception ex)
+                {
+                    Logger.Instance.LogFormat(LogType.Error, this, Properties.Resources.NetworkError, ex);
+                    Logger.Instance.LogException(this, ex);
+                }
+            }
+        }
+
+        internal void Stop()
+        {
+            if (_clientSocket != null)
+            {
+                _clientSocket.Close();
+            }
+
+            _serverSocket.Stop();
+            IsRunning = false;
+        }
+
+        #endregion
+    }
+}
---
+++ AlarmSources/Network/Server/HandleAlarmClient.cs
@@ -0,0 +1,105 @@
+﻿// This file is part of AlarmWorkflow.
+// 
+// AlarmWorkflow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AlarmWorkflow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with AlarmWorkflow.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using AlarmWorkflow.Shared.Diagnostics;
+using System.IO;
+using AlarmWorkflow.Shared.Core;
+
+namespace AlarmWorkflow.AlarmSource.Network.Server
+{
+    class HandleAlarmClient
+    {
+        #region Constants
+
+        private const int BufferSize = 1024;
+
+        #endregion
+
+        #region Fields
+
+        private readonly UTF8Encoding _encoding;
+        private TcpClient _clientSocket;
+        private NetworkAlarmSource _parent;
+
+        #endregion
+
+        #region Constructors
+
+        private HandleAlarmClient()
+        {
+            _encoding = new UTF8Encoding();
+        }
+
+        #endregion
+
+        #region Methods
+
+        internal static void StartClient(TcpClient clientSocket, NetworkAlarmSource parent)
+        {
+            if (clientSocket == null)
+            {
+                throw new ArgumentNullException("clientSocket");
+            }
+            if (parent == null)
+            {
+                throw new ArgumentNullException("parent");
+            }
+
+            HandleAlarmClient handleAlarmClient = new HandleAlarmClient();
+            handleAlarmClient._clientSocket = clientSocket;
+            handleAlarmClient._parent = parent;
+
+            Thread ctThread = new Thread(handleAlarmClient.ReceiveThread);
+            ctThread.Name = Properties.Resources.NetworkParserError;
+            ctThread.Priority = ThreadPriority.BelowNormal;
+            ctThread.IsBackground = true;
+            ctThread.Start();
+        }
+
+        private void ReceiveThread()
+        {
+            byte[] buffer = new byte[BufferSize];
+            try
+            {
+                using (NetworkStream networkStream = _clientSocket.GetStream())
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    int numBytesRead;
+                    while ((numBytesRead = networkStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        ms.Write(buffer, 0, numBytesRead);
+                    }
+
+                    string alarmJson = _encoding.GetString(ms.ToArray(), 0, (int)ms.Length);
+                    Operation operation = Json.Deserialize<Operation>(alarmJson);
+
+                    _parent.PushIncomingAlarm(operation);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Logger.Instance.LogFormat(LogType.Error, this, Properties.Resources.NetworkParserError, ex);
+                Logger.Instance.LogException(this, ex);
+            }
+        }
+        #endregion
+
+    }
+}
