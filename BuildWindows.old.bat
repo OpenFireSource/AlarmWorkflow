3e6338f74b51c902c490680356dde8c14554949a
chrishensel <futurefwarrior@googlemail.com>
2015-12-12 12:59:08 +0100

    Updated build scripts to use the new MSBuild

--- BuildWindows.old.bat
+++ BuildWindows.old.bat
@@ -1,8 +1,11 @@
 @echo off
 rem -------------------------------------------------
 rem AlarmWorkflow build script (DEBUG)
+rem 
+rem Please change "Framework64" to "Framework" in the following path
+rem if you're running this script on a x86 or 32-bit OS.
 rem -------------------------------------------------
-SET build="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+SET build=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe
 
 echo -------------------------------------------------
 echo Build submodules...
