f8b6bc3fe35edd1fca90287b467eb8ca4c97165b
chrishensel <futurefwarrior@googlemail.com>
2016-01-22 17:13:47 +0100

    Added password editor for ConfEd
    This avoids exposing set passwords as plain text, as well as prohibiting copying passwords.

--- Configuration/Configuration/TypeEditors/StringTypeEditor.xaml.cs
+++ Configuration/Configuration/TypeEditors/StringTypeEditor.xaml.cs
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with AlarmWorkflow.  If not, see <http://www.gnu.org/licenses/>.
 
+using System;
 using System.Windows.Controls;
 using AlarmWorkflow.Shared.Core;
 using AlarmWorkflow.Windows.ConfigurationContracts;
@@ -26,9 +27,16 @@ namespace AlarmWorkflow.Windows.Configuration.TypeEditors
     [ConfigurationTypeEditor(typeof(System.String))]
     public partial class StringTypeEditor : UserControl, ITypeEditor
     {
+        #region Constants
+
+        private const string PasswordEditorParameter = "Password";
+
+        #endregion
+
         #region Fields
 
         private bool _isNull;
+        private ITextEditor _editor;
 
         #endregion
 
@@ -53,12 +61,12 @@ namespace AlarmWorkflow.Windows.Configuration.TypeEditors
         {
             get
             {
-                if (_isNull && string.IsNullOrEmpty(txtValue.Text))
+                if (_isNull && string.IsNullOrEmpty(_editor.Value))
                 {
                     return null;
                 }
 
-                return txtValue.Text;
+                return _editor.Value;
             }
             set
             {
@@ -66,7 +74,7 @@ namespace AlarmWorkflow.Windows.Configuration.TypeEditors
                 // We need to remember null since the textbox turns it into ""
                 _isNull = v == null;
 
-                txtValue.Text = v;
+                _editor.Value = v;
             }
         }
 
@@ -80,6 +88,76 @@ namespace AlarmWorkflow.Windows.Configuration.TypeEditors
 
         void ITypeEditor.Initialize(string editorParameter)
         {
+            bool isPassword = string.Equals(editorParameter, PasswordEditorParameter, StringComparison.Ordinal);
+
+            if (isPassword)
+            {
+                _editor = new PasswordEditor();
+            }
+            else
+            {
+                _editor = new StringEditor();
+            }
+
+            text.Content = _editor.Visual;
+        }
+
+        #endregion
+
+        #region Nested types
+
+        interface ITextEditor
+        {
+            Control Visual { get; }
+            string Value { get; set; }
+        }
+
+        class StringEditor : ITextEditor
+        {
+            #region Fields
+
+            private TextBox _control = new TextBox();
+
+            #endregion
+
+            #region ITextEditor Members
+
+            Control ITextEditor.Visual
+            {
+                get { return _control; }
+            }
+
+            string ITextEditor.Value
+            {
+                get { return _control.Text; }
+                set { _control.Text = value; }
+            }
+
+            #endregion
+        }
+
+        class PasswordEditor : ITextEditor
+        {
+            #region Fields
+
+            private PasswordBox _control = new PasswordBox();
+
+            #endregion
+
+            #region ITextEditor Members
+
+            Control ITextEditor.Visual
+            {
+                get { return _control; }
+            }
+
+            string ITextEditor.Value
+            {
+                get { return _control.Password; }
+                set { _control.Password = value; }
+            }
+
+            #endregion
         }
 
         #endregion
