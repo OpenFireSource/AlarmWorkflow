34962a7357fd92348eefab55407a0bd20e07ee83
F4lcon <kevin@stieglitz-online.de>
2018-11-12 22:05:55 +0100

    Add ALARMiator

---
+++ Jobs/Engine/Alarmiator/Helpers.cs
@@ -0,0 +1,58 @@
+﻿// This file is part of AlarmWorkflow.
+// 
+// AlarmWorkflow is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// AlarmWorkflow is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with AlarmWorkflow.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AlarmWorkflow.Job.Alarmiator
+{
+    internal class Helper
+    {
+        private static RijndaelManaged GetRijndaelManaged(string secretKey)
+        {
+            byte[] keyBytes = new byte[16];
+            byte[] secretKeyBytes = Encoding.UTF8.GetBytes(secretKey);
+            Array.Copy(secretKeyBytes, keyBytes, Math.Min(keyBytes.Length, secretKeyBytes.Length));
+            return new RijndaelManaged
+            {
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7,
+                KeySize = 128,
+                BlockSize = 128,
+                Key = keyBytes,
+                IV = keyBytes
+            };
+        }
+
+        private static byte[] Encrypt(byte[] plainBytes, RijndaelManaged rijndaelManaged)
+        {
+            return rijndaelManaged.CreateEncryptor().TransformFinalBlock(plainBytes, 0, plainBytes.Length);
+        }
+
+        /// <summary>
+        /// Encrypts plaintext using AES 128bit key and a Chain Block Cipher and returns a base64 encoded string
+        /// </summary>
+        /// <param name="plainText">Plain text to encrypt</param>
+        /// <param name="key">Secret key</param>
+        /// <returns>Base64 encoded string</returns>
+        internal static string Encrypt(string plainText, string key)
+        {
+            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(Encrypt(plainBytes, GetRijndaelManaged(key)));
+        }
+    }
+}
+
