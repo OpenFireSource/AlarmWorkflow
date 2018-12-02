595889ef043abb0760362a91b7c0cac78ce498f9
F4lcon <kevin@stieglitz-online.de>
2018-12-02 18:47:13 +0100

    Fix BuildTools

--- BuildTools/BuildWindows.Release.bat
+++ BuildTools/BuildWindows.Release.bat
@@ -11,21 +11,5 @@ echo Restoring .nuget Packages
 CALL RestorePackages.bat
 
 echo -------------------------------------------------
-echo Build Shared...
-CALL %build% %root%\Shared\Shared.sln /p:Configuration=Release /verbosity:minimal
-
-echo -------------------------------------------------
-echo Build Windows-specific stuff...
-CALL %build% %root%\Backend\Backend.sln /p:Configuration=Release /verbosity:minimal
-CALL %build% %root%\BackendServices\BackendServices.sln /p:Configuration=Release /verbosity:minimal
-
-CALL %build% %root%\AlarmSources\AlarmSources.sln /p:Configuration=Release /verbosity:minimal
-
-CALL %build% %root%\Windows\Windows.sln /p:Configuration=Release /verbosity:minimal
-CALL %build% %root%\WindowsUIWidgets\WindowsUIWidgets.sln /p:Configuration=Release /verbosity:minimal
-CALL %build% %root%\Configuration\Configuration.sln /p:Configuration=Release /verbosity:minimal
-
-CALL %build% %root%\Jobs\Engine\EngineJobs.sln /p:Configuration=Release /verbosity:minimal
-CALL %build% %root%\Jobs\WindowsUI\WindowsUIJobs.sln /p:Configuration=Release /verbosity:minimal
-
-CALL %build% %root%\Tools\Tools.sln /p:Configuration=Release /verbosity:minimal
+echo Build AlarmWorkflow ...
+CALL %build% %root%\AlarmWorkflow.sln /p:Configuration=Release /verbosity:minimal
